5c781e0fa1e85f34c6a6316d349ee1b395745fc8
Xooncraft <daniel@demeter.li>
2026-06-18 19:58:20 +0200

    Daniel fertig

--- GitAufgabe/Main.java
+++ GitAufgabe/Main.java
@@ -2,6 +2,13 @@ package GitAufgabe;
 
 public class Main {
      public static void main(String[] args){
-         
+
+          Daniel meinHandy = new Daniel("Samsung Galaxy S23 FE", 800.0);
+
+          System.out.println(meinHandy.toString());
+
+          double endPreis = meinHandy.berechneRabatt(10.0);
+
+          System.out.println("Preis nach Rabatt: " + endPreis + "$");
      }
 }
