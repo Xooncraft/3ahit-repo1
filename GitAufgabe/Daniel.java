a66e39668b167dc7c8281c688fc7027e88d4ff87
Xooncraft <daniel@demeter.li>
2026-06-18 19:55:27 +0200

    update

--- GitAufgabe/Daniel.java
+++ GitAufgabe/Daniel.java
@@ -1,4 +1,34 @@
 package GitAufgabe;
 
 public class Daniel {
+
+    private String name;
+    private double preis;
+
+    public Daniel(String name, double preis){
+        this.name = name;
+        this.preis = preis;
+    }
+
+    public double berechneRabatt(double rabbatInProzent){
+        double abzug = this.preis * (rabbatInProzent / 100);
+        return this.preis - abzug;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPreis(){
+        return preis;
+    }
+
+    public void setPreis(double preis){
+        this.preis = preis;
+    }
+
+    @Override
+    public String toString(){
+        return "Produkt: " + name + " kostet " + preis + "$";
+    }
 }
---
+++ GitAufgabe/Main.java
@@ -0,0 +1,7 @@
+package GitAufgabe;
+
+public class Main {
+     public static void main(String[] args){
+         
+     }
+}
